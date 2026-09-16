e31c481f2320d6ea589254c7511b7c9d912967ce
Ubuntu <ubuntu@ip-172-31-25-22.ap-northeast-1.compute.internal>
2026-09-16 07:38:50 +0000

    Add associations, validations, and enum settings

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,17 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :orders
+  has_many :addresses
+  has_many :cart_items
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
+  validates :address, presence: true
+  validates :phone_number, presence: true
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,20 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  has_many :order_details, dependent: :destroy
+
+    enum :payment_method, { credit_card: 0, transfer: 1 }
+    enum :status, { 
+    pending_payment: 0, 
+    payment_confirm: 1, 
+    in_production: 2, 
+    preparing_shipment: 3, 
+    shipped: 4 
+  }
+  
+  validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :shipping_cost, presence: true
+  validates :total_payment, presence: true
+
 end
