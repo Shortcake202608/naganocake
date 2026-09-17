ca753e1e500b5b1a3bc82803d7f74489a6f0b0f6
Ubuntu <ubuntu@ip-172-31-25-22.ap-northeast-1.compute.internal>
2026-09-17 04:38:49 +0000

    Add admin login UI

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ end
 
 gem "devise"
 gem "enum_help"
+
+gem "json", "~> 2.7"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_09_16_053304) do
+ActiveRecord::Schema[8.0].define(version: 2026_09_17_020846) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,6 +47,18 @@ ActiveRecord::Schema[8.0].define(version: 2026_09_16_053304) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_addresses_on_customer_id"
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "cart_items", force: :cascade do |t|
